2501c23767bd6493172aea991fec718b8b97e290
slideon <adamkasztenny@gmail.com>
2016-06-21 21:37:20 -0400

    Add basic config json parsing

--- src/main/scala/org/scrawler/Scrawler.scala
+++ src/main/scala/org/scrawler/Scrawler.scala
@@ -1,9 +1,14 @@
 package org.scrawler
 
+import play.api.libs.json.Json
+
 object Scrawler {
     def main(args: Array[String]): Unit = {        
-        val jsonConfigFile = scala.io.Source.fromFile(args(0)).mkString
+        val rawJsonConfig = scala.io.Source.fromFile(args(0)).mkString
 
-        
+        val jsonConfig = Json.parse(rawJsonConfig)
+       
+        val seed: Seq[String] = (jsonConfig \ "seed").as[Seq[String]]
+        val databaseURI = (jsonConfig \ "dburi").as[String]
     }
 }
