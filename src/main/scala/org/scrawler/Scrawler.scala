689d083c5ba34a8ef773fab7f7d8e37101621d8e
slideon <adamkasztenny@gmail.com>
2016-06-25 10:49:15 -0400

    Add basic support for different databases

--- src/main/scala/org/scrawler/Scrawler.scala
+++ src/main/scala/org/scrawler/Scrawler.scala
@@ -1,31 +1,34 @@
 package org.scrawler
 
+import org.scrawler.db.DatabaseConnection
 import play.api.libs.json.Json
-
 import org.scrawler.queue.Queue
 import org.scrawler.db.mongodb.MongoDBDatabaseConnection
 
 object Scrawler {
-    var mongoDBDatabaseConnection: MongoDBDatabaseConnection = null
+    var databaseConnection: DatabaseConnection = null
 
     def main(args: Array[String]): Unit = {
         val rawJsonConfig = scala.io.Source.fromFile(args(0)).mkString
 
         val jsonConfig = Json.parse(rawJsonConfig)
-       
+
         val seed: Set[String] = (jsonConfig \ "seed").as[Set[String]]
 
         val databaseURI = (jsonConfig \ "dburi").as[String]
         val databaseName = (jsonConfig \ "dbname").as[String]
+        val databaseType = (jsonConfig \ "dbtype").as[String]
 
-        mongoDBDatabaseConnection = new MongoDBDatabaseConnection(databaseURI, databaseName)
+        databaseType match {
+            case "mongodb" => databaseConnection = new MongoDBDatabaseConnection (databaseURI, databaseName)
+        }
 
         val queue = new Queue(seed)
 
         runScrawler(queue)
     }
 
-    def databaseConnection = mongoDBDatabaseConnection
+    def getDatabaseConnection = databaseConnection
     
     private def runScrawler(queue: Queue) = while(!queue.empty) { queue.getFromQueueAndAddLinked }
 }
--- src/main/scala/org/scrawler/website/WebPage.scala
+++ src/main/scala/org/scrawler/website/WebPage.scala
@@ -4,5 +4,5 @@ import org.scrawler.Scrawler
 import java.net.URL
 
 case class WebPage(title: String, url: URL, body: String, keywords: Set[String]) {
-    Scrawler.databaseConnection.saveWebPageToDatabase(this)
+    Scrawler.getDatabaseConnection.saveWebPageToDatabase(this)
 }
