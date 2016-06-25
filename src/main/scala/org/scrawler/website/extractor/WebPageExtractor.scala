62daab783a778edd0f90cd33700985106647a856
slideon <adamkasztenny@gmail.com>
2016-06-25 11:36:51 -0400

    Refactor WebPageExtractor a bit and add a method to get the values of all headers.

--- src/main/scala/org/scrawler/website/extractor/WebPageExtractor.scala
+++ src/main/scala/org/scrawler/website/extractor/WebPageExtractor.scala
@@ -24,16 +24,19 @@ object WebPageExtractor {
             }     
         }
 
-        val headers1 = getElements(driver, "h1")
-        val headers2 = getElements(driver, "h2")
+        val headers = getHeaders(driver)
         val bold = getElements(driver, "b") ++ getElements(driver, "strong")
 
-        val keywords = (bold ++ headers1 ++ headers2 ++ title.split(" ").toSet)
-        val sanitizedKeywords = keywords.map(_.split(" ").toSet).flatten.map(_.toLowerCase).map({keyword: String => keyword.replace(",", "").replace(".", "")})        
+        val keywords = bold ++ headers ++ title.split(" ").toSet
+        val sanitizedKeywords = keywords.flatMap(_.split(" ").toSet).map(_.toLowerCase).map({
+          keyword: String => keyword.replace(",", "").replace(".", "")
+        })
 
         new WebPage(title, url, body, sanitizedKeywords)
    }
 
+    private def getHeaders(driver: HtmlUnitDriver): Set[String] =
+      (for (i <- 1 to 6) yield getElements(driver, s"h$i")).toSet.flatten
 
     private def getElements(driver: HtmlUnitDriver, element: String) = {
         try { 
