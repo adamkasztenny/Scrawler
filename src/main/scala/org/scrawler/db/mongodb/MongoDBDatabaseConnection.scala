9517382917b89f87c447b7eb656f624f95369524
slideon <adamkasztenny@gmail.com>
2016-06-26 23:22:04 -0400

    Add support for fancier mongo uris

--- src/main/scala/org/scrawler/db/mongodb/MongoDBDatabaseConnection.scala
+++ src/main/scala/org/scrawler/db/mongodb/MongoDBDatabaseConnection.scala
@@ -1,20 +1,20 @@
 package org.scrawler.db.mongodb
 
-import org.scrawler.db.DatabaseConnection
-import org.scrawler.website.WebPage
-
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import java.util.Calendar
 
 import com.mongodb.casbah.Imports._
-import java.util.Calendar 
+import org.scrawler.db.DatabaseConnection
+import org.scrawler.website.WebPage
+import org.slf4j.LoggerFactory
 
 // thanks to http://mongodb.github.io/casbah/3.1/getting-started/ for all the help!
 
 class MongoDBDatabaseConnection(uri: String, dbName: String) extends DatabaseConnection(uri, dbName) {
     val logger = LoggerFactory.getLogger(this.getClass)
 
-    val mongoClient: MongoClient = MongoClient(uri) 
+    // thanks http://stackoverflow.com/questions/32322166/mongoexception-on-url-format-when-attempting-connection-to-mongolab#34723804
+    val mongoClientURI = new MongoClientURI(uri)
+    val mongoClient: MongoClient = MongoClient(mongoClientURI) 
 
     def saveWebPageToDatabase(webPage: WebPage): Unit = {
         val db = mongoClient(dbName)
