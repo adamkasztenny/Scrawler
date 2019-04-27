bec7e95a13dfb829e912a0d855dc8d2635bffa7e
Adam Kasztenny <adamkasztenny@gmail.com>
2019-04-26 20:40:12 -0400

    Improve tests for WebPageExtractor

--- src/test/scala/org/scrawler/website/extractor/WebPageExtractorTest.scala
+++ src/test/scala/org/scrawler/website/extractor/WebPageExtractorTest.scala
@@ -7,6 +7,7 @@ import org.scalacheck.Gen
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FlatSpec, Matchers}
 import org.scrawler.Sample._
+import org.scrawler.domain.WebPage
 
 import scala.collection.JavaConverters._
 
@@ -31,35 +32,49 @@ class WebPageExtractorTest extends FlatSpec with Matchers with MockFactory {
     val headers: Seq[String] = Seq.fill(6)(words)
     (1 to 6).foreach(index =>
       (driver.findElements(_: By)) expects By.xpath(s"//h$index") returning webElements(headers(index - 1)))
+
+    val webPage: WebPage = WebPageExtractor(url)
   }
 
   "extracting a web page" should "include a title, if the page has one" in new Fixture {
-    WebPageExtractor(url).title shouldBe title
+    webPage.title shouldBe title
   }
 
   it should "include a blank title, if the page has none" in new Fixture {
     override val title = null
-    WebPageExtractor(url).title shouldBe ""
+    webPage.title shouldBe ""
   }
 
   it should "include the URL" in new Fixture {
-    WebPageExtractor(url).url shouldBe url
+    webPage.url shouldBe url
   }
 
   it should "include the body" in new Fixture {
-    WebPageExtractor(url).body shouldBe body
+    webPage.body shouldBe body
   }
 
   it should "include bold and strong text in the keywords" in new Fixture {
-    WebPageExtractor(url).keywords should contain allElementsOf asKeywords(bold) ++ asKeywords(strong)
+    webPage.keywords should contain allElementsOf asKeywords(bold) ++ asKeywords(strong)
   }
 
   it should "include header text in the keywords" in new Fixture {
-    WebPageExtractor(url).keywords should contain allElementsOf headers.flatMap(asKeywords)
+    webPage.keywords should contain allElementsOf headers.flatMap(asKeywords)
   }
 
   it should "include the title in the keywords" in new Fixture {
-    WebPageExtractor(url).keywords should contain allElementsOf asKeywords(title)
+    webPage.keywords should contain allElementsOf asKeywords(title)
+  }
+
+  "keywords" should "not include spaces" in new Fixture {
+    webPage.keywords.find(_.contains(" ")) shouldBe empty
+  }
+
+  they should "not include dots" in new Fixture {
+    webPage.keywords.find(_.contains(".")) shouldBe empty
+  }
+
+  they should "not include commas" in new Fixture {
+    webPage.keywords.find(_.contains(",")) shouldBe empty
   }
 
   private def webElements(text: String): java.util.List[WebElement] = List(webElement(text)).asJava
@@ -70,7 +85,7 @@ class WebPageExtractorTest extends FlatSpec with Matchers with MockFactory {
     element
   }
 
-  private def asKeywords(string: String): Seq[String] = string.toLowerCase.split(" ")
+  private def asKeywords(string: String): Seq[String] = string.toLowerCase.replace(".", "").replace(",", "").split(" ")
 
-  private def words: String = s"${sample(Gen.alphaStr)} ${sample(Gen.alphaStr)}"
+  private def words: String = s"${sample(Gen.asciiStr)} ${sample(Gen.asciiStr)}"
 }
