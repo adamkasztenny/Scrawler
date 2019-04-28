d5b14b2a53c6fd9b0e20d5990b77ecd1c79469c5
Adam Kasztenny <adamkasztenny@gmail.com>
2019-04-28 12:45:55 -0400

    "Happy path" tests for LinkExtractor

--- src/test/scala/org/scrawler/website/extractor/LinkExtractorTest.scala
+++ src/test/scala/org/scrawler/website/extractor/LinkExtractorTest.scala
@@ -1,14 +1,32 @@
 package org.scrawler.website.extractor
 
-import org.openqa.selenium.WebDriver
+import java.net.URL
+
+import org.openqa.selenium.{By, WebDriver, WebElement}
+import org.scalacheck.Gen
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{FunSuite, Matchers}
 import org.scrawler.domain.UrlGenerator
+import org.scrawler.Sample._
+import scala.collection.JavaConverters._
 
 class LinkExtractorTest extends FunSuite with Matchers with MockFactory {
 
   trait Fixture {
     implicit val driver: WebDriver = mock[WebDriver]
+  }
+
+  trait LinksPresent extends Fixture {
+    val url: URL = UrlGenerator()
+    (driver.get(_: String)) expects url.toString
+
+    val links: Seq[String] = Seq(Gen.alphaStr, Gen.alphaStr).map(link => s"https://${sample(link)}")
+    val firstLink = mock[WebElement]
+    (firstLink.getAttribute(_: String)) expects "href" returning links.head
+    val secondLink = mock[WebElement]
+    (secondLink.getAttribute(_: String)) expects "href" returning links.last
+
+    (driver.findElements(_: By)) expects By.xpath("//a") returning List(firstLink, secondLink).asJava
   }
 
   test("should return empty set for email links") {
@@ -33,6 +51,12 @@ class LinkExtractorTest extends FunSuite with Matchers with MockFactory {
   test("should return empty set for text files") {
     new Fixture {
       LinkExtractor(UrlGenerator(".txt")) shouldBe empty
+    }
+  }
+
+  test("should return a list of links on the page") {
+    new LinksPresent {
+      LinkExtractor(url) should contain theSameElementsAs links.map(new URL(_))
     }
   }
 }
